304760c92cd026a007da4c3d56519e4a2e0c224b
tonygiang <tony_s_giang@yahoo.com>
2022-07-31 18:16:58 +0700

    Project push.

---
+++ IEnumerable.MinMaxBy.cs
@@ -0,0 +1,258 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+using System.Collections.Generic;
+
+namespace CLSS
+{
+  public static partial class IEnumerableMinMaxBy
+  {
+    /// <summary>
+    /// Returns the maximum value in a generic sequence according to a specified
+    /// key selector function.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <typeparam name="TKey">The type of key to compare elements by.
+    /// </typeparam>
+    /// <param name="source">A sequence of values to determine the maximum value
+    /// of.</param>
+    /// <param name="keySelector">A function to extract the key for each
+    /// element.</param>
+    /// <returns>The value with the maximum key in the sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="keySelector"/> is null.</exception>
+    /// <exception cref="InvalidOperationException"><paramref name="source"/>
+    /// contains no element.</exception>
+    public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource> source,
+      Func<TSource, TKey> keySelector)
+      where TKey : IComparable<TKey>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (keySelector == null) throw new ArgumentNullException("keySelector");
+      bool hasValue = false;
+      var currentMax = default(TSource);
+      var currentMaxKey = default(TKey);
+      foreach (TSource e in source)
+      {
+        if (!hasValue)
+        {
+          var selectedKey = keySelector(e);
+          if (selectedKey.CompareTo(currentMaxKey) > 0)
+          { currentMaxKey = selectedKey; currentMax = e; }
+        }
+        else
+        { currentMaxKey = keySelector(e); currentMax = e; hasValue = true; }
+      }
+      if (hasValue) return currentMax;
+      throw new InvalidOperationException("Sequence contains no elements");
+    }
+
+    /// <summary>
+    /// Returns the maximum value in a generic sequence according to a specified
+    /// key selector function and key comparer.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <typeparam name="TKey">The type of key to compare elements by.
+    /// </typeparam>
+    /// <param name="source">A sequence of values to determine the maximum value
+    /// of.</param>
+    /// <param name="keySelector">A function to extract the key for each
+    /// element.</param>
+    /// <param name="comparer">The <see cref="IComparer{T}"/> to compare keys.
+    /// </param>
+    /// <returns>The value with the maximum key in the sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/>,
+    /// <paramref name="keySelector"/> or <paramref name="comparer"/> is null.
+    /// </exception>
+    /// <exception cref="InvalidOperationException"><paramref name="source"/>
+    /// contains no element.</exception>
+    public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource> source,
+      Func<TSource, TKey> keySelector,
+      IComparer<TKey> comparer)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (keySelector == null) throw new ArgumentNullException("keySelector");
+      if (comparer == null) throw new ArgumentNullException("comparer");
+      bool hasValue = false;
+      var currentMax = default(TSource);
+      var currentMaxKey = default(TKey);
+      foreach (TSource e in source)
+      {
+        if (!hasValue)
+        {
+          var selectedKey = keySelector(e);
+          if (comparer.Compare(selectedKey, currentMaxKey) > 0)
+          { currentMaxKey = selectedKey; currentMax = e; }
+        }
+        else
+        { currentMaxKey = keySelector(e); currentMax = e; hasValue = true; }
+      }
+      if (hasValue) return currentMax;
+      throw new InvalidOperationException("Sequence contains no elements");
+    }
+
+    /// <summary>
+    /// Returns the minimum value in a generic sequence according to a specified
+    /// key selector function.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <typeparam name="TKey">The type of key to compare elements by.
+    /// </typeparam>
+    /// <param name="source">A sequence of values to determine the minimum value
+    /// of.</param>
+    /// <param name="keySelector">A function to extract the key for each
+    /// element.</param>
+    /// <returns>The value with the minimum key in the sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="keySelector"/> is null.</exception>
+    /// <exception cref="InvalidOperationException"><paramref name="source"/>
+    /// contains no element.</exception>
+    public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source,
+      Func<TSource, TKey> keySelector)
+      where TKey : IComparable<TKey>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (keySelector == null) throw new ArgumentNullException("keySelector");
+      bool hasValue = false;
+      var currentMin = default(TSource);
+      var currentMinKey = default(TKey);
+      foreach (TSource e in source)
+      {
+        if (!hasValue)
+        {
+          TKey selectedKey = keySelector(e);
+          if (selectedKey.CompareTo(currentMinKey) < 0)
+          { currentMinKey = selectedKey; currentMin = e; }
+        }
+        else
+        { currentMinKey = keySelector(e); currentMin = e; hasValue = true; }
+      }
+      if (hasValue) return currentMin;
+      throw new InvalidOperationException("Sequence contains no elements");
+    }
+
+    /// <summary>
+    /// Returns the minimum value in a generic sequence according to a specified
+    /// key selector function and key comparer.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <typeparam name="TKey">The type of key to compare elements by.
+    /// </typeparam>
+    /// <param name="source">A sequence of values to determine the minimum value
+    /// of.</param>
+    /// <param name="keySelector">A function to extract the key for each
+    /// element.</param>
+    /// <param name="comparer">The <see cref="IComparer{T}"/> to compare keys.
+    /// </param>
+    /// <returns>The value with the minimum key in the sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/>,
+    /// <paramref name="keySelector"/> or <paramref name="comparer"/> is null.
+    /// </exception>
+    /// <exception cref="InvalidOperationException"><paramref name="source"/>
+    /// contains no element.</exception>
+    public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source,
+      Func<TSource, TKey> keySelector,
+      IComparer<TKey> comparer)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (keySelector == null) throw new ArgumentNullException("keySelector");
+      if (comparer == null) throw new ArgumentNullException("comparer");
+      bool hasValue = false;
+      var currentMin = default(TSource);
+      var currentMinKey = default(TKey);
+      foreach (TSource e in source)
+      {
+        if (!hasValue)
+        {
+          var selectedKey = keySelector(e);
+          if (comparer.Compare(selectedKey, currentMinKey) < 0)
+          { currentMinKey = selectedKey; currentMin = e; }
+        }
+        else
+        { currentMinKey = keySelector(e); currentMin = e; hasValue = true; }
+      }
+      if (hasValue) return currentMin;
+      throw new InvalidOperationException("Sequence contains no elements");
+    }
+
+    /// <summary>
+    /// Returns the maximum value in a generic sequence according to a specified
+    /// key selector function.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">A sequence of values to determine the maximum value
+    /// of.</param>
+    /// <param name="keySelector">A function to extract the key for each
+    /// element.</param>
+    /// <returns>The value with the maximum key in the sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="keySelector"/> is null.</exception>
+    /// <exception cref="InvalidOperationException"><paramref name="source"/>
+    /// contains no element.</exception>
+    public static TSource MaxBy<TSource>(this IEnumerable<TSource> source,
+      Func<TSource, IComparable> selector)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (selector == null) throw new ArgumentNullException("keySelector");
+      bool hasValue = false;
+      var currentMax = default(TSource);
+      var currentMaxKey = default(IComparable);
+      foreach (TSource e in source)
+      {
+        if (!hasValue)
+        {
+          var selectedKey = selector(e);
+          if (selectedKey.CompareTo(currentMaxKey) > 0)
+          { currentMaxKey = selectedKey; currentMax = e; }
+        }
+        else
+        { currentMaxKey = selector(e); currentMax = e; hasValue = true; }
+      }
+      if (hasValue) return currentMax;
+      throw new InvalidOperationException("Sequence contains no elements");
+    }
+
+    /// <summary>
+    /// Returns the minimum value in a generic sequence according to a specified
+    /// key selector function.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">A sequence of values to determine the minimum value
+    /// of.</param>
+    /// <param name="keySelector">A function to extract the key for each
+    /// element.</param>
+    /// <returns>The value with the minimum key in the sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="keySelector"/> is null.</exception>
+    /// <exception cref="InvalidOperationException"><paramref name="source"/>
+    /// contains no element.</exception>
+    public static TSource MinBy<TSource>(this IEnumerable<TSource> source,
+      Func<TSource, IComparable> keySelector)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (keySelector == null) throw new ArgumentNullException("keySelector");
+      bool hasValue = false;
+      var currentMin = default(TSource);
+      var currentMinKey = default(IComparable);
+      foreach (TSource e in source)
+      {
+        if (!hasValue)
+        {
+          var selectedKey = keySelector(e);
+          if (selectedKey.CompareTo(currentMinKey) < 0)
+          { currentMinKey = selectedKey; currentMin = e; }
+        }
+        else
+        { currentMinKey = keySelector(e); currentMin = e; hasValue = true; }
+      }
+      if (hasValue) return currentMin;
+      throw new InvalidOperationException("Sequence contains no elements");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,56 @@
+﻿# CLSS.ExtensionMethods.IEnumerable.MinMaxBy
+
+### Problem
+
+[`MinBy`](https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.minby) & [`MaxBy`](https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.maxby) are new LINQ extension methods first introduced in .NET 6. Visit their reference documentation links to learn what they do. These methods have the following drawbacks:
+
+- They are not available in .NET runtime environments earlier than .NET 6, which are most of the .NET runtime environments.
+- They do not statically ensure that key types implement the [`IComparable`](https://docs.microsoft.com/en-us/dotnet/api/system.icomparable) or [`IComparable<T>`](https://docs.microsoft.com/en-us/dotnet/api/system.icomparable-1) interface. It is possible to pass in a selector of invalid type and get no compile-time error, but you will get a runtime exception.
+- They don't check for null key selector.
+- They don't check for empty source collection.
+
+### Solution
+
+This package backports `MinBy` and `MaxBy` for .NET versions before .NET 6 that are also compatible with .NET Standard 1.0. Learn more about compatible runtime environments [here](https://dotnet.microsoft.com/en-us/platform/dotnet-standard#versions). In the process, they also rectified the above drawbacks.
+
+Statically-checked type:
+
+```
+using CLSS;
+
+public struct CustomType
+{
+  // System.Uri does not implement IComparable or IComparable<T>
+  public System.Uri Schema;
+}
+
+var collection = new CustomType[]
+{
+  new CustomType { Schema = new System.Uri("uri1") },
+  new CustomType { Schema = new System.Uri("uri2") },
+  new CustomType { Schema = new System.Uri("uri3") }
+};
+// Compilation error with CLSS version of MaxBy
+var max = collection.MaxBy(e => e.Schema);
+```
+
+Null-checked key selector:
+
+```
+using CLSS;
+
+var numbers = new int[] { 6, 3, 17 };
+var max = numbers.MaxBy(null); // Throws ArgumentNullException
+```
+
+Empty-checked source collection:
+```
+using CLSS;
+
+var timestampts = new System.DateTime[0];
+var max = numbers.MaxBy(t => t.Year); // Throws InvalidOperationException
+```
+
+Although this package's `MinBy` and `MaxBy` statically ensure that the key selector returns an `IComparable` or `IComparable<T>`, passing in an optional custom `IComparer<T>` will loosen this requirement and allow key selectors that return any type. 
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
